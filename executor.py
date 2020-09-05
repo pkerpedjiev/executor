54c3037b7f8b0bd46bd88dc14f3c4941d4220b1b
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2020-09-04 21:54:21 -0400

    Executor with steps

--- executor.py
+++ executor.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
 import argparse
+import hashlib
 import json
 import os
 import os.path as op
 import subprocess as sp
 import sys
+from hashlib import sha256
 
 import docker
 
@@ -13,7 +15,10 @@ import slugid
 
 DATA_DIR='fetch'
 
-def extract_urls(command):
+def fetch_dir(base_dir):
+    return op.join(base_dir, '.fetch')
+
+def extract_urls(command, base_dir):
     """Extract all the urls and replace them with slugids
     so that we can download the files locally."""
     parts = command.split(' ')
@@ -21,48 +26,75 @@ def extract_urls(command):
     for i,part in enumerate(parts):
         if (part.startswith("https://") or part.startswith("http://") or part.startswith("http://")):
             filename = part.split('/')[-1]
-            to_fetch[part] = f"{DATA_DIR}/{filename}"
+            to_fetch[part] = op.join('.fetch',filename)
             parts[i] = to_fetch[part]
 
     return parts, to_fetch
 
-def fetch_file(url):
+def fetch_file(url, base_dir):
     sp.call([
-        'wget', '-NP', 'fetch', url
+        'wget', '-NP', fetch_dir(base_dir), url
     ])
 
+def save_steps(steps, base_dir):
+    steps_file = op.join(base_dir, '.steps')
+
+    with open(steps_file, 'w') as f:
+        json.dump(steps, f)
+
+def load_steps(base_dir):
+    steps_file = op.join(base_dir, '.steps')
+
+    if op.exists(steps_file):
+        with open(steps_file, 'r') as f:
+            steps = json.load(f)
+            return steps
+    return None
 
 def main():
     parser = argparse.ArgumentParser(description="""
 
-    python executor.py conf.json
+    python executor.py conf_dir
 """)
 
-    parser.add_argument('conf')
+    parser.add_argument('conf_dir')
     #parser.add_argument('-o', '--options', default='yo',
     #                    help="Some option", type='str')
     #parser.add_argument('-u', '--useless', action='store_true',
     #                    help='Another useless option')
 
     args = parser.parse_args()
+    base_dir = args.conf_dir
 
-    with open(args.conf) as f:
-        conf = json.load(f)
+    with open(op.join(args.conf_dir, 'conf.cut')) as f:
+        conf = f.readlines()
 
-    if not op.exists('fetch'):
-        os.makedirs('fetch')
+    fetch_dir = op.join(args.conf_dir, 'fetch')
+    if not op.exists(fetch_dir):
+        os.makedirs(fetch_dir)
 
-    if not op.exists('output'):
-        os.makedirs('output')
+    steps = load_steps(base_dir)
 
-    [cmd_parts, to_fetch] = extract_urls(conf['command'])
-    filenames = {}
+    for i,line in enumerate(conf):
+        prior_commands = "".join(conf[:i+1])
+        cmd_hash = sha256(prior_commands.encode('utf8')).hexdigest()
+        if cmd_hash in steps and steps[cmd_hash]:
+            print("skipping", line)
+            continue
 
-    for url,filename in to_fetch.items():
-        fetch_file(url)
+        [cmd_parts, to_fetch] = extract_urls(line.strip(), base_dir)
+        filenames = {}
 
-    print("cmd_parts", cmd_parts)
-    sp.call(cmd_parts)
+        for url,filename in to_fetch.items():
+            fetch_file(url, base_dir)
+
+        # print("cmd_parts", cmd_parts)
+        cmd = " ".join(cmd_parts)
+        sp.call(cmd, shell=True, cwd=args.conf_dir)
+
+        steps[cmd_hash] = True
+        save_steps(steps, base_dir)
+        # sp.call(cmd_parts)
     # for filename in conf['fetch']:
     #     sp.call([
     #         'wget', '-PN', 'fetch', filename["url"],
@@ -73,4 +105,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
