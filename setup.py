ab9238670177e7e8c199c3edae098c3d23372052
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2021-02-21 12:45:25 -0800

    Updated .gitignore and setup.py and added __init__.py

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
+import io
+import os
+
 from setuptools import setup
 
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+def read(*parts, **kwargs):
+    filepath = os.path.join(HERE, *parts)
+    encoding = kwargs.pop("encoding", "utf-8")
+    with io.open(filepath, encoding=encoding) as fh:
+        text = fh.read()
+    return text
+
+
+def get_requirements(path):
+    content = read(path)
+    return [req for req in content.split("\n") if req != "" and not req.startswith("#")]
+
+
+install_requires = get_requirements("requirements.txt")
+
 setup(
-    name='executor',
-    version='0.1',
-    py_modules=['executor'],
-    install_requires=[
-        'Click',
-    ],
-    entry_points='''
+    name="executor",
+    version="0.1",
+    py_modules=["executor"],
+    packages=["executor"],
+    install_requires=["Click",],
+    entry_points="""
         [console_scripts]
         executor=executor.cli:cli
-    ''',
+    """,
 )
